78c5626099c3c8edeef51dadd88c7cabe8ca7a1a
HammadNadeem21 <nhammad424@gmail.com>
2024-11-13 02:55:29 +0500

    updated

---
+++ portfolio-tailwind-css/components/sidebar.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+
+export function Sidebar() {
+  return (
+    <div className="pb-12">
+      <div className="space-y-4 py-4">
+       hello
+        
+      </div>
+    </div>
+  )
+}
---
+++ portfolio-tailwind-css/src/app/page.tsx
@@ -0,0 +1,28 @@
+
+import Image from "next/image";
+import MainPage from "../../components/mainPage";
+import Navbar from "../../components/navbar";
+import { Sidebar } from "../../components/sidebar";
+
+
+export default function Home() {
+
+ 
+  return (
+    
+      <div>
+<MainPage/>
+     
+
+
+        {/* <div className="w-1/5">
+          <Sidebar/>
+        </div>
+
+        <div className="w-full">
+        <Navbar/>
+        </div> */}
+      </div>  
+    
+  );
+}
